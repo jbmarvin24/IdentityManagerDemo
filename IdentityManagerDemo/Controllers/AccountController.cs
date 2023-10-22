2617dfac81d649a6e995f25a6838f57a0be34c5f
John Marvin Doniña <jbmarvin24@gmail.com>
2023-10-22 10:34:48 +0800

    Added registration logic

--- IdentityManagerDemo/Controllers/AccountController.cs
+++ IdentityManagerDemo/Controllers/AccountController.cs
@@ -1,10 +1,19 @@
 ﻿using IdentityManagerDemo.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityManagerDemo.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager=userManager;
+            this.signInManager=signInManager;
+        }
         public IActionResult Index()
         {
             return View();
@@ -15,6 +24,40 @@ namespace IdentityManagerDemo.Controllers
         {
             RegisterViewModel registerViewModel = new();
             return View(registerViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email= model.Email,
+                    Name = model.Name
+                };
+
+                var result = await userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+
+                AddErrors(result);
+            }
+
+            return View(model);
+        }
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
         }
     }
 }
