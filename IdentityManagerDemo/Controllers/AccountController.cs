01f9a8766248af690aa7192619f2d4abf049d184
John Marvin Doniña <jbmarvin24@gmail.com>
2023-10-22 10:09:52 +0800

    Added registration view

---
+++ IdentityManagerDemo/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using IdentityManagerDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityManagerDemo.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Register()
+        {
+            RegisterViewModel registerViewModel = new();
+            return View(registerViewModel);
+        }
+    }
+}
