5c7e987dccfb3af543b0d39f6e5fbbc9e24dea2a
John Marvin Doniña <jbmarvin24@gmail.com>
2023-10-25 20:39:31 +0800

    Added users controller

---
+++ IdentityManagerDemo/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using IdentityManagerDemo.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityManagerDemo.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext db;
+        private readonly UserManager<IdentityUser> userManager;
+
+        public UserController(ApplicationDbContext db, UserManager<IdentityUser> userManager)
+        {
+            this.db=db;
+            this.userManager=userManager;
+        }
+        public IActionResult Index()
+        {
+            var userList = db.ApplicationUser.ToList();
+            var userRole = db.UserRoles.ToList();
+            var roles = db.Roles.ToList();
+
+            foreach (var user in userList)
+            {
+                var role = userRole.FirstOrDefault(u => u.UserId == user.Id);
+                if (role == null)
+                {
+                    user.Role = "None";
+                }
+                else
+                {
+                    user.Role = roles.FirstOrDefault(u => u.Id == role.RoleId).Name;
+                }
+            }
+
+            return View(userList);
+        }
+    }
+}
